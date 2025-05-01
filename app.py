bd0c8471f62a8440e510bc11b2c142eae898d5c0
asheema <sssuhana19@gmail.com>
2025-05-01 18:09:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 from streamlit_option_menu import option_menu
-#from streamlit_extras.metric_cards import style_metric_cards
+from streamlit_extras.metric_cards import style_metric_cards
 
 # Load Lottie animation from URL
 #def load_lottie_url(url: str):
@@ -84,4 +84,4 @@ elif selected == "About":
     """)
 
 # Style metrics
-#style_metric_cards()
+style_metric_cards()
