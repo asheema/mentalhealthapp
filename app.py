3bc696fdb9c3f0e94e791e03acce4881b0e71f36
asheema <sssuhana19@gmail.com>
2025-05-01 17:57:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit_lottie import st_lottie
 import requests
 from streamlit_option_menu import option_menu
 #from streamlit_extras.metric_cards import style_metric_cards
